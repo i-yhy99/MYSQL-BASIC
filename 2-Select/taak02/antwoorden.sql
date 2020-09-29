e071808c0fd2f2262379724cd9a16137fc44accf
Ilias el Yahyaoui <yahyaouii@rocva.nl>
2020-09-29 08:34:22 +0200

    SQL query's geoefend

--- 2-Select/taak02/antwoorden.sql
+++ 2-Select/taak02/antwoorden.sql
@@ -9,11 +9,14 @@
 -- Opdracht 5
 
 -- Opdracht 6 
-
+SELECT country
+FROM jaar2016
+WHERE rank = 25
 -- Opdracht 7 
-
+SELECT country FROM jaar2016 where score > 7087
 -- Opdracht 8
-
+SELECT country, region FROM jaar2016 WHERE rank <= 5
 -- Opdracht 9
-
+SELECT score FROM jaar2015 WHERE country = "Ireland"
 -- Opdracht 10
+SELECT country FROM jaar2016 WHERE region = "Latin America and Caribbean"
